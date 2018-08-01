a71d6625eb2eb2a82aa8d303598e655aeacb54f7
ABuarque <abuarquemf@gmail.com>
2018-08-01 00:56:40 -0300

    new

--- data_structures/class_codes/QuickSort.c
+++ data_structures/class_codes/QuickSort.c
@@ -8,29 +8,34 @@ void swap(int* a, int* b) {
 	*b = t;
 }
 
-void quickSort(int v[], int start, int end) {
+void quickSort(int v[], int start, int end, int shouldSortIncreasing) {
 	if(start >= end) 
 		return;
 	int left = start;
 	int right = end;
 	int pivot = v[(left + right) / 2];
 	while(left <= right) {
-		while(v[left] < pivot) left++; 
-		while(v[right] > pivot) right--;
+		if(shouldSortIncreasing) {
+			while(v[left] < pivot) left++; 
+			while(v[right] > pivot) right--;
+		} else {
+			while(v[left] > pivot) left++; 
+			while(v[right] < pivot) right--;
+		}
 		if(left <= right) {
 			swap(&v[left], &v[right]);
 			left++;
 			right--;
 		}
 	}
-	quickSort(v, start, right);
-	quickSort(v, left, end);
+	quickSort(v, start, right, shouldSortIncreasing);
+	quickSort(v, left, end, shouldSortIncreasing);
 }
 
 int main() {
 	int x[] = {1,-2,3,-11,3, 33, 100, -100, 3,4,5};
 	int size = sizeof(x) / sizeof(x[0]);
-	quickSort(x, 0, size - 1);
+	quickSort(x, 0, size - 1, 0);
 	for(int i = 0; i < size; i++)
 		printf("%d ", x[i]);
 	printf("\n");
